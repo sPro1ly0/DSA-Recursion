cab0083667f7886f38b69ddf56b889f4823cbfed
Sabrina Becker <sbecka8@gmail.com>
2020-03-20 13:46:26 -0400

    Add work on problems 5-7

--- recursion-problems.js
+++ recursion-problems.js
@@ -70,4 +70,65 @@ const triangle = function(num) {
     return num + triangle(num - 1);
 }
 
-// console.log(triangle(4));
+// console.log(triangle(1));
+
+// 5. String Splitter
+// What is the input to the program? '02/20/2020'
+// What is the output of the program?  ["02", "20", "2020"]
+// What is the input to each recursive call? "20/2020", "2020"
+// What is the output of each recursive call? "02", "20", "2020"
+
+const stringSplitter = function(str) {
+    let slash = str.search('/');
+    let splitOutSlash = str.slice(0, slash);
+
+    if (slash == -1) {
+        splitOutSlash = str;
+        return [ splitOutSlash ];
+    }
+
+    return [ splitOutSlash, ...stringSplitter(str.slice(slash + 1)) ];
+}
+
+// console.log(stringSplitter('02/20/2020'));
+
+// 6. Fibonacci
+// What is the input to the program? number
+// What is the output of the program?  [0, 1, 1, 2, 3, 5, 8, 13]
+// What is the input to each recursive call? number - 1
+// What is the output of each recursive call? 0, 1, 1, 2, 3, 5, 8, 13, ...
+
+let fibonacci = function(num) {
+    
+    if (num === 1) {
+        return [0, 1];
+    } else {
+        let arr = fibonacci(num - 1);
+        arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+        return arr;
+    }
+
+}
+
+// console.log(fibonacci(7));
+
+// 7. Factorial
+// What is the input to the program? number, 5
+// What is the output of the program? 120
+// What is the input to each recursive call? (num - 1) OR (num + 1)
+// What is the output of each recursive call? 4, 3, 2, 1, 0
+
+const factorial = function(num) {
+
+    if (num === 0) {
+        return 1;
+    } else if (Math.sign(num) === -1) { // for negative numbers
+        return num * factorial(num + 1);
+    } else if (Math.sign(num) === 1) { // for positive numbers
+        return num * factorial(num - 1);
+    }
+}
+
+// console.log(factorial(5));
+// console.log(factorial(-3));
+
